e9c412d24c96c34bf72d169828336c7b7a89f6f2
bolcal <emrebolcal@gmail.com>
2020-02-26 19:03:59 +0300

    initial commit

---
+++ StatsCrawler/BCrawler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace StatsCrawler
+{
+    public static class BCrawler
+    {
+
+        public static void GetFromWeb(string url)
+        {
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+            List<HtmlNode> hnodes = doc.DocumentNode.Descendants("table").ToList();
+            var tblNode = doc.DocumentNode.Descendants("table").Select(tbl => tbl.Attributes["id"].Value == "tblFixture");
+        }
+    }
+}
